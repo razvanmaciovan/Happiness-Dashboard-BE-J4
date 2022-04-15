753778958d43b06e363f5ecfe1fde65c1152fd7b
Tenche Robert <teencherobert@gmail.com>
2022-04-15 12:03:06 +0300

    Rename app from "DemoApplication" to "IBM_HD_BE"

--- src/main/java/com/example/demo/IBM_HD_BE.java
+++ src/main/java/com/example/demo/IBM_HD_BE.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class IBM_HD_BE {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(IBM_HD_BE.class, args);
 	}
 
 }
