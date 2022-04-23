d308eeb7ca18e37a1e203a8964fcabc9841be955
Tenche Robert <teencherobert@gmail.com>
2022-04-23 13:33:04 +0300

    Enabled swagger
    Renamed SpringFoxConfig to SwaggerApplication

--- src/main/java/com/example/demo/Controller/ApiControllers.java
+++ src/main/java/com/example/demo/Controller/ApiControllers.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @RestController
+@EnableSwagger2
 public class ApiControllers {
     @Autowired
     private UserRepo userRepo;
