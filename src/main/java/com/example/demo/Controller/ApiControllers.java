a6d733099f932e19c831ab5759c08a36006fc4aa
Tenche Robert <teencherobert@gmail.com>
2022-04-23 18:35:18 +0300

    Added CORS

--- src/main/java/com/example/demo/Controller/ApiControllers.java
+++ src/main/java/com/example/demo/Controller/ApiControllers.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:8080")
 public class ApiControllers {
     @Autowired
     private UserRepo userRepo;
