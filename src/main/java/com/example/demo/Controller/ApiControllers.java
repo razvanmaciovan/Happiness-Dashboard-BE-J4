6a9257d8dc1d633aca417103d15ab668cce06c91
Tenche Robert <teencherobert@gmail.com>
2022-04-23 16:01:53 +0300

    FEATURE: search user by username

--- src/main/java/com/example/demo/Controller/ApiControllers.java
+++ src/main/java/com/example/demo/Controller/ApiControllers.java
@@ -2,11 +2,11 @@ package com.example.demo.Controller;
 
 import com.example.demo.Models.User;
 import com.example.demo.repo.UserRepo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +21,31 @@ public class ApiControllers {
     public List<User> getUsers() {
 
         return userRepo.findAll();
+    }
+
+    /*
+     *  function: User getUserById(String id)
+     *
+     *  description:
+     *
+     */
+    @ApiOperation("Returns user by specified username")
+    @ApiParam("Username")
+    @GetMapping(value = "/users/{username}")
+    public User getUserById(@PathVariable String username) {
+
+        // get all users as a stream
+        User user = getUsers().stream()
+                // filter by id
+                .filter(usr -> username.contentEquals(usr.getUsername()))
+                // return first value or return null
+                .findFirst().orElse(null);
+        /*
+         *  NOTE:   this operation should ALWAYS find only one user
+         *          since the id's are unique in the database
+         *          ( only if the user exists )
+         */
+        return user;
     }
 
     @PostMapping(value = "/register")
