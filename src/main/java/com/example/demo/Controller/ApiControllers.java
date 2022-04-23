2b4c18c907e1e108838eff97146448aa2ff3237c
Maciovan Razvan <razvan.maciovan02@e-uvt.ro>
2022-04-23 18:40:41 +0300

    Update ApiControllers.java
    Updated the CORS localhost to match with the frontend one

--- src/main/java/com/example/demo/Controller/ApiControllers.java
+++ src/main/java/com/example/demo/Controller/ApiControllers.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ApiControllers {
     @Autowired
     private UserRepo userRepo;
