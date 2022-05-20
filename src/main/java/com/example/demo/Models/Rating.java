7782d4e26d9da7e7d78057b100c970f1968ec6af
Mercescu Ionut <ionut.mercescu00@e-uvt.ro>
2022-05-20 18:05:05 +0300

    Poll returns ID aswell

--- src/main/java/com/example/demo/Models/Rating.java
+++ src/main/java/com/example/demo/Models/Rating.java
@@ -5,20 +5,15 @@ import java.util.Date;
 
 @Entity
 public class Rating {
-
     @Id
-
     @GeneratedValue
     private long id;
     private long pollId;
-
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "userId", referencedColumnName = "id", nullable = false)
     private User user;
-
     @Column
     private int grade;
-
     private Date timeStamp;
 
     public long getPollId() {
